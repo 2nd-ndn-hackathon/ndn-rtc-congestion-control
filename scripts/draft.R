313fdbcce876faca5aae39096ac5ee8ef0f68fc0
schneiderklaus <klaus@email.arizona.edu>
2016-03-21 10:28:28 -0700

    renamed day2 stuff

--- scripts/draft.R
+++ scripts/draft.R
@@ -6,57 +6,84 @@ library(reshape2)
 
 setwd("/home/klaus/ccn/ndn-rtc-cc/scripts")
 
-
 # theme_set(theme_bw(base_size = 12, base_family = "CM Sans"))
 theme_set(theme_bw() + theme( strip.background=element_blank(), legend.key = element_rect(color="white"), panel.background=element_blank(), 
                               panel.border=element_blank(), axis.line = element_line(size=0.3)))
 
 # Read in data
-data = read.table("../results/run3/output3.csv", header=T)
 
-summary(data)
+filename <- "output7_c1_2.csv"
+
+data = read.table(paste("../results/day2/",filename, sep=""), header=T)
+
+data$rtt <- data$rtt_prime_ms - data$Dgen_ms
 
 
 
-g.rtt <- ggplot(data, aes (x=rts, y=rtt_prime)) +
+g.rtt.prime <- ggplot(data, aes (x=rts_ms/1000, y=rtt_prime_ms)) +
   geom_line(size=0.8) + 
+  xlab("Time [s]") +
   ylab("RTT [ms]") +
-  ggtitle("RTT")
+  ggtitle("RTT Prime")
   #facet_wrap(~ Node) 
 
-g.rtt_est <- ggplot(data, aes (x=rts, y=rtt_est)) +
+# g.rtt_est <- ggplot(data, aes (x=rts_ms/1000, y=rtt_est_ms)) +
+#   geom_line(size=0.8) + 
+#   xlab("Time [s]") +
+#   ylab("RTT [ms]") +
+#   ggtitle("RTT Est")
+# #facet_wrap(~ Node) 
+
+
+g.rtt <- ggplot(data, aes (x=rts_ms/1000, y=rtt)) +
   geom_line(size=0.8) + 
-  ylab("RTT [ms]") +
-  ggtitle("RTT Est")
+  xlab("Time [s]") +
+  ylab("Dgen [ms]") +
+  ggtitle("RTT")
 #facet_wrap(~ Node) 
 
 
-g.dgen <- ggplot(data, aes (x=rts, y=Dgen)) +
+g.dgen <- ggplot(data, aes (x=rts_ms/1000, y=Dgen_ms)) +
   geom_line(size=0.8) + 
+  xlab("Time [s]") +
   ylab("Dgen [ms]") +
   ggtitle("Data Generation Delay")
 #facet_wrap(~ Node) 
 
 
-g.buf <- ggplot(data, aes (x=rts, y=buf_play)) +
+g.darr <- ggplot(data, aes (x=rts_ms/1000, y=Darr_ms)) +
   geom_line(size=0.8) + 
+  xlab("Time [s]") +
+  ylab("Dgen [ms]") +
+  ggtitle("Data Inter-Arrival Delay")
+#facet_wrap(~ Node) 
+
+
+
+g.buf <- ggplot(data, aes (x=rts_ms/1000, y=buf_play_ms)) +
+  geom_line(size=0.8) + 
+  xlab("Time [s]") +
   ylab("Buffer size [ms]") +
   ggtitle("Playout Buffer")
 #facet_wrap(~ Node) 
 
 
+g.lambdad <- ggplot(data, aes (x=rts_ms/1000, y=lambda_d)) +
+  geom_line(size=0.8) + 
+  xlab("Time [s]") +
+  ylab("Lambda [ms]") +
+  ggtitle("Lambda (Cwnd)")
+#facet_wrap(~ Node) 
 
+pdf(paste("../graphs/",filename,".pdf", sep=""), useDingbats=T, width=12)
 g.rtt
-g.rtt_est
-
+g.rtt.prime
 g.dgen
-
-
-pdf("../graphs/draft.pdf", useDingbats=T, width=12)
-g.rtt
-g.rtt_est
-g.dgen
+g.darr
 g.buf
+g.lambdad
 dev.off()
 #embed_fonts("../graphs/rates.pdf")
+
+summary(data$run)
 
